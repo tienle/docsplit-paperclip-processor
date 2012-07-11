afc41472442acc68f29ebee19faf08c4fb89d0c1
Geoff Harcourt <geoff.harcourt@gmail.com>
2012-07-11 12:09:31 -0400

    Specs for DocsplitPdf processor.

--- spec/docsplit_pdf_spec.rb
+++ spec/docsplit_pdf_spec.rb
@@ -1,27 +1,60 @@
 require 'spec_helper'
 
 describe Paperclip::DocsplitPdf do
-  describe "converts documents to PDF" do
-    it "converts .docx documents to PDF" do
-      pending
+  def make_output
+    Paperclip::DocsplitPdf.new(@file).make
+  end
+
+  context "with a Microsoft Word .docx file" do
+    before(:all) do
+      @file = File.open("./fixtures/word_xml.docx")
+      @output = make_output
     end
-    
-    it "performs no conversion if the document is already a PDF" do
-      pending
+
+    after(:all) do
+      @file.close
     end
-    
-    it "raises an error if PDF conversion was unsuccessful" do
-      pending
+
+    describe "produces a file with a .pdf extension" do
+      it "names the converted document with a .pdf extension" do
+        File.extname(@output.path).should eq(".pdf")
+      end
+
+      it "produces PDF-format file" do
+        FileMagic.new.file(@output.path).should =~ /pdf/i
+      end
     end
   end
   
-  describe "knows whether or not a document is in PDF format" do
+  it "passes the document on if it is already a PDF" do
+    @file = File.open("./fixtures/portable_document_format.pdf")
+
+    lambda {
+      FileMagic.new.file(make_output.path).should =~ /pdf/i
+    }.should_not raise_error
+    
+    @file.close
+  end
+
+  it "raises an error if PDF conversion was unsuccessful" do
+    @file = File.open("./fixtures/not_a_docx.docx")
+    
+    lambda {
+      make_output
+    }.should raise_error
+    
+    @file.close
+  end
+
+  describe "#pdf_format?" do
     it "identifies a PDF document as a PDF" do
-      pending
+      processor = Paperclip::DocsplitPdf.new(File.open("./fixtures/portable_document_format.pdf"))
+      processor.pdf_format?.should be_true
     end
-  
+
     it "identifies a non-PDF document as not a PDF" do
-      pending
+      processor = Paperclip::DocsplitPdf.new(File.open("./fixtures/word_xml.docx"))
+      processor.pdf_format?.should be_false
     end
   end
 end
