ce9d667f77bc54c592c5a9be852370e7c62e4929
Geoff Harcourt <geoff.harcourt@gmail.com>
2012-07-11 00:22:56 -0400

    Initial pending specs for docsplit_pdf processor.

---
+++ spec/docsplit_pdf_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Paperclip::DocsplitPdf do
+  describe "converts documents to PDF" do
+    it "converts .docx documents to PDF" do
+      pending
+    end
+    
+    it "performs no conversion if the document is already a PDF" do
+      pending
+    end
+    
+    it "raises an error if PDF conversion was unsuccessful" do
+      pending
+    end
+  end
+  
+  describe "knows whether or not a document is in PDF format" do
+    it "identifies a PDF document as a PDF" do
+      pending
+    end
+  
+    it "identifies a non-PDF document as not a PDF" do
+      pending
+    end
+  end
+end
