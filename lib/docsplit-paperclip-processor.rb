c1aed878e60e3ee638dc43136479b7ade4457dac
Geoff Harcourt <geoff.harcourt@gmail.com>
2012-07-11 12:08:34 -0400

    Added #src_path and refactored #make and #pdf_format? to avoid unnecessary argument passing.

--- lib/docsplit-paperclip-processor.rb
+++ lib/docsplit-paperclip-processor.rb
@@ -31,28 +31,33 @@ module Paperclip
   class DocsplitPdf < DocsplitProcessor
     def make
       begin
-        src_path = File.expand_path(@src.path)
         dst_dir = Dir.tmpdir
         dst_path = File.join(dst_dir, "#{@basename}.pdf")
 
-        if pdf_format?(src_path)
+        if pdf_format?
           dst_path = File.join(dst_dir, "_#{@basename}.pdf")
           FileUtils.copy_file(src_path, dst_path)
         else
           Docsplit.extract_pdf(src_path, :output => dst_dir)
         end
       rescue Exception => e
+        raise e
+        
         Rails.logger.error e.message
         raise Paperclip::Error, "There was an error converting #{@basename} to pdf"
       end
       File.open(dst_path)
     end
 
-    def pdf_format?(file)
+    def pdf_format?
       file_magic = FileMagic.new
-      type = file_magic.file(file)
+      type = file_magic.file(src_path)
       file_magic.close
       type =~ /pdf/i
+    end
+    
+    def src_path
+      File.expand_path(@src.path)
     end
   end
 
