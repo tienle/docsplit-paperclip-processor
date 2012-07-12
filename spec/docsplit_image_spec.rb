515cf238ebf92856ecc84a4887a32c19837eed8e
Geoff Harcourt <geoff.harcourt@gmail.com>
2012-07-12 17:20:28 -0400

    Initial DocsplitImage spec.

---
+++ spec/docsplit_image_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Paperclip::DocsplitImage do
+  it "#make generates an image for each page of the document" do
+    pending
+  end
+  
+  it "#make generates images at the specified resolution" do
+    pending
+  end
+  
+  it "#make generates images in the specified format" do
+    pending
+  end
+  
+  it "#make returns the image of the first page" do
+    pending
+  end
+  
+  it "#make raises an error if the processing was unsuccessful" do
+    pending
+  end
+end
