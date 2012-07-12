7d37faf5c9691215d9b152cf6a062665114a7381
Geoff Harcourt <geoff.harcourt@gmail.com>
2012-07-12 18:55:56 -0400

    Hooks for docsplit_image spec.

--- spec/docsplit_image_spec.rb
+++ spec/docsplit_image_spec.rb
@@ -1,29 +1,43 @@
 require 'spec_helper'
 
 describe Paperclip::DocsplitImage do
-  it "#make generates an image for each page of the document" do
-    pending
-  end
-  
-  it "#make generates images at the specified resolution" do
-    pending
-  end
-  
-  it "#make generates images in the specified format" do
-    pending
-  end
-  
-  it "#make returns the image of the first page" do
-    pending
-  end
-  
-  it "#make raises an error if the processing was unsuccessful" do
-    @file = File.open("./fixtures/not_a_docx.docx")
+  context "with a valid pdf file attachment" do
+    before(:all) do
+      @file = File.open("./fixtures/nmfa-press-release-7-12-02.pdf")
+      @processor = Paperclip::DocsplitImage.new(@file, {:format => :jpg})
+      @output = @processor.make
+    end
     
-    lambda {
-      Paperclip::DocsplitImage.new(@file).make
-    }.should raise_error(Paperclip::Error)
+    after(:all) do
+      @file.close
+    end
     
-    @file.close
+    it "#make generates an image for each page of the document" do
+      pending
+    end
+  
+    it "#make generates images at the specified resolution" do
+      pending
+    end
+  
+    it "#make generates images in the specified format" do
+      pending
+    end
+  
+    it "#make returns the image of the first page" do
+      pending
+    end
+  end
+  
+  context "with a bad file attachment" do
+    it "#make raises an error if the processing was unsuccessful" do
+      @file = File.open("./fixtures/not_a_docx.zip")
+    
+      lambda {
+        Paperclip::DocsplitImage.new(@file, {:format => :jpg}).make
+      }.should raise_error(Paperclip::Error)
+    
+      @file.close
+    end
   end
 end
