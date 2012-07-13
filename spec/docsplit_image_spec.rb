8a5dd67abd7b8e6e514d7a759d2f6a0711670578
Geoff Harcourt <geoff.harcourt@gmail.com>
2012-07-13 08:48:33 -0400

    Test coverage for extract_image functionality.

--- spec/docsplit_image_spec.rb
+++ spec/docsplit_image_spec.rb
@@ -1,10 +1,18 @@
 require 'spec_helper'
 
 describe Paperclip::DocsplitImage do
+  def pdf_jpg_images
+    Dir.entries(Dir.tmpdir).reject{ |x| !(x =~ /twopage_\d.jpg/) }
+  end
+  
   context "with a valid pdf file attachment" do
     before(:all) do
-      @file = File.open("./fixtures/nmfa-press-release-7-12-02.pdf")
-      @processor = Paperclip::DocsplitImage.new(@file, {:format => :jpg})
+      pdf_jpg_images.each do |tempfile|
+        File.delete(File.join(Dir.tmpdir, tempfile))
+      end
+      
+      @file = File.open("./fixtures/twopage.pdf")
+      @processor = Paperclip::DocsplitImage.new(@file, {:format => :jpg, :size => "50x50"})
       @output = @processor.make
     end
     
@@ -13,25 +21,29 @@ describe Paperclip::DocsplitImage do
     end
     
     it "#make generates an image for each page of the document" do
-      pending
+      pdf_jpg_images.count.should eq(2)
     end
   
     it "#make generates images at the specified resolution" do
-      pending
+      cmd = %Q[identify -format "%wx%h" "#{@output.path}"]
+      `#{cmd}`.chomp.should eq("39x50")
     end
   
     it "#make generates images in the specified format" do
-      pending
+      pdf_jpg_images.each do |output_file|
+        FileMagic.new.file(File.join(Dir.tmpdir, output_file)).should =~ /jpeg/i
+      end
     end
   
     it "#make returns the image of the first page" do
-      pending
+      File.basename(@output).should eq('twopage_1.jpg')
     end
   end
   
-  context "with a bad file attachment" do
+  context "when processing fails" do
     it "#make raises an error if the processing was unsuccessful" do
-      @file = File.open("./fixtures/not_a_docx.zip")
+      @file = File.open("./fixtures/twopage.pdf")
+      Dir.stub!(:tmpdir).and_return(:raise)
     
       lambda {
         Paperclip::DocsplitImage.new(@file, {:format => :jpg}).make
