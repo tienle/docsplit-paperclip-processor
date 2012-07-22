872262cdc0b3b3e47d0fea9fb8d934cf5671a837
Geoff Harcourt <geoff.harcourt@gmail.com>
2012-07-22 18:56:42 -0400

    Loading AR with Sqlite3 DB in memory.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,16 @@
-require "paperclip"
-require "filemagic"
-require "docsplit"
-require "docsplit-paperclip-processor"
+require 'paperclip'
+require 'filemagic'
+require 'docsplit'
+require 'docsplit-paperclip-processor'
 
 require 'rspec'
 require 'rspec/autorun'
+
+# Prepare activerecord
+require "active_record"
+
+# Connect to sqlite
+ActiveRecord::Base.establish_connection(
+  "adapter" => "sqlite3",
+  "database" => ":memory:"
+)
