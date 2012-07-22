7bb2b94fe977daaf103c1340e2b07f4f4f023b98
Geoff Harcourt <geoff.harcourt@gmail.com>
2012-07-22 19:11:56 -0400

    Reorganizing into contexts for AR testing.

--- spec/docsplit_extract_text_spec.rb
+++ spec/docsplit_extract_text_spec.rb
@@ -3,22 +3,33 @@ require 'spec_helper'
 describe Paperclip::DocsplitText do
   before(:all) do
     @file = File.open("./fixtures/word_xml.docx")
-    @options = {}
-    @processor = Paperclip::DocsplitText.new(File.open("./fixtures/word_xml.docx"), @options)
   end
 
   after(:all) do
     @file.close
   end
 
-  it "#make sends the correct commands to Docsplit" do
-    Docsplit.should_receive(:extract_text).with(File.expand_path(@file.path), @options.merge(:output => Dir.tmpdir))
+  context "with no options supplied" do
+    before(:all) do
+      @options = {}
+      @processor = Paperclip::DocsplitText.new(@file, @options)
+    end
 
-    @processor.make
-  end
+    it "#make sends the correct commands to Docsplit" do
+      Docsplit.should_receive(:extract_text).with(File.expand_path(@file.path), @options.merge(:output => Dir.tmpdir))
+  
+      @processor.make
+    end
     
-  it "#make returns the full text of the document" do
-    @processor.make.should eq("This is a test document.\n\n\f")
+    it "#make returns the full text of the document" do
+      @processor.make.should eq("This is a test document.\n\n\f")
+    end
+  end
+
+  context "with a destination column for extracted text" do
+    it "#make stores the full text in the specified field" do
+      pending
+    end
   end
 
   context "when processing fails" do
@@ -26,7 +37,7 @@ describe Paperclip::DocsplitText do
       Dir.stub!(:tmpdir).and_return(:raise)
     
       lambda {
-        @processor.make
+        Paperclip::DocsplitText.new(@file, {}).make
       }.should raise_error(Paperclip::Error)
     end
   end
